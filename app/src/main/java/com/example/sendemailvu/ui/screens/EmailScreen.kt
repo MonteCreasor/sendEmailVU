2f8b749cb5d6d40c1f84a55cc1f9c51dc46f375d
Creasor <montecreasor@gmail.com>
2023-10-10 22:52:20 -0400

    Fixed soft keyboard offset issue.

--- app/src/main/java/com/example/sendemailvu/ui/screens/EmailScreen.kt
+++ app/src/main/java/com/example/sendemailvu/ui/screens/EmailScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.isImeVisible
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
