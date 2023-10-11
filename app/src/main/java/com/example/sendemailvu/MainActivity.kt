06231869a282a8c49dbf8ed27aa29ba01c0a449a
Creasor <montecreasor@gmail.com>
2023-10-10 23:43:24 -0400

    Added comments

--- app/src/main/java/com/example/sendemailvu/MainActivity.kt
+++ app/src/main/java/com/example/sendemailvu/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.example.sendemailvu.ui.composables.NavAppScreen
 import com.example.sendemailvu.ui.composables.homeScreenRoute
-import com.example.sendemailvu.ui.screens.EmailScreen
 import com.example.sendemailvu.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +23,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AppTheme {
-//                EmailScreen()
                 NavAppScreen(startDestination = homeScreenRoute)
             }
         }
